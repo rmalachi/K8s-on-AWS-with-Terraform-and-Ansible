9cd905d17b6e79645721923bf45d173ab9daddc2
rmalachi <malachiron@gmail.com>
2024-07-13 14:54:19 +0300

    Add vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,107 @@
+# -------------------------------------------------------------------
+
+# Create a VPC
+resource "aws_vpc" "tfrm-vpc" {
+  cidr_block           = "20.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    name = "dev"
+  }
+}
+
+resource "aws_subnet" "tfrm-public-subnet" {
+  vpc_id                  = aws_vpc.tfrm-vpc.id
+  cidr_block              = "20.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+/* 
+resource "aws_internet_gateway" "tfrm-gw" {
+  vpc_id = aws_vpc.tfrm-vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "tfrm-public-rt" {
+  vpc_id = aws_vpc.tfrm-vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default-route" {
+  route_table_id         = aws_route_table.tfrm-public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.tfrm-gw.id
+}
+
+resource "aws_route_table_association" "tfrm-public-assoc" {
+  subnet_id      = aws_subnet.tfrm-public-subnet.id
+  route_table_id = aws_route_table.tfrm-public-rt.id
+}
+
+resource "aws_security_group" "tfrm-sg" {
+  name        = "dev-sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.tfrm-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "tfrm-auth" {
+  key_name   = "tfrm-key"
+  public_key = file("C:\\Users\\malac\\.ssh\\tfrm-key.pub")
+}
+
+resource "aws_instance" "dev-node" {
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.tfrm-auth.id
+  vpc_security_group_ids = [aws_security_group.tfrm-sg.id]
+  subnet_id              = aws_subnet.tfrm-public-subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+}
+*/
+
+# -------------------------------------------------------------------
+
+
+
+
+
+
+
+
 # Launch master node
 resource "aws_instance" "k8s_master" {
   ami           = var.ami["master"]
